30513bc185b42f1a3cb3774981f60b0912790095
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2022-01-10 17:18:12 +0000

    php script to access the database

---
+++ cycle_logging.php
@@ -0,0 +1,99 @@
+<html>
+<head>
+  <title>Cycle Rides Results</title>
+</head>
+<body>
+<h1>Cycle Rides Results</h1>
+<?php
+
+  // create short variable names
+  $searchtype=$_POST['searchtype'];
+  $startdate=trim($_POST['startdate']);
+  $enddate=trim($_POST['enddate']);
+
+  if (!$searchtype || !$startdate || !$enddate) {
+     echo 'You have not entered search details.  Please go back and try again.';
+     exit;
+  }
+
+  if (!get_magic_quotes_gpc()){
+    $searchtype = addslashes($searchtype);
+    $startdate = addslashes($startdate);
+    $enddate = addslashes($enddate);
+  }
+  echo "<p>searchtype: ".$searchtype."</p>";
+  switch ($searchtype) {
+   case "Days":
+     $searchtype="fahrrad_rides";
+     $extratxt="Day";
+     $datekey="Date";
+     break;
+  case "Weeks":
+     $searchtype="fahrrad_weekly_summary";
+     $extratxt="Week";
+     $datekey="Week_starting_on";
+     break;
+  case "Months":
+     $searchtype="fahrrad_monthly_summary";
+     $extratxt="Month";
+     $datekey="Month_starting_on";
+     break;
+  case "Years":
+     $searchtype="fahrrad_yearly_summary";
+     $extratxt="Year";
+     $datekey="Year_starting_on";
+     break;
+  default:
+     echo 'Somthing went wrong.  Please go back and try again.';
+     exit;
+  }
+  $mysql_Settings = [
+     "host" => 'localhost',
+     "user" => "yourusername",
+     "password" => "yourpassword",
+     "db" => "",
+  ];
+  // better: save it as variable in another script and include that script: https://stackoverflow.com/questions/17020651/how-to-hide-protect-password-details-in-php/17021458#17021458
+  $myfile = fopen("../fahrrad_mysql.params", "r") or die("Unable to open file!");
+  while(!feof($myfile)) {
+     $arrayString = explode("=", fgets($myfile) );
+     //Print_r($arrayString);
+     if (count($arrayString)==2) {
+        $mysql_Settings[trim($arrayString[0])] = trim($arrayString[1]);
+     }
+  }
+  fclose($myfile);
+
+  @ $db = new mysqli($mysql_Settings["host"], $mysql_Settings["user"], $mysql_Settings["password"], $mysql_Settings["db"]);
+  if (mysqli_connect_errno()) {
+     echo 'Error: Could not connect to database.  Please try again later.';
+     exit;
+  }
+
+  $query = "SELECT ".$datekey." AS Date, ".$extratxt."KM AS KM, ".$extratxt."Seconds AS Seconds, ".$extratxt."KMH AS KMH FROM ".$searchtype." WHERE ".$datekey." BETWEEN '".$startdate."' AND '".$enddate."'";
+  //echo "<p>Query: ".$query."</p>";
+  $result = $db->query($query);
+
+  $num_results = $result->num_rows;
+
+  echo "<p>Number of entries found: ".$num_results."</p>";
+
+  for ($i=0; $i <$num_results; $i++) {
+     $row = $result->fetch_assoc();
+     echo "<p><strong>".($i+1).". Date: ";
+     echo htmlspecialchars(stripslashes($row['Date']));
+     echo "</strong><br />Distance [km]: ";
+     echo stripslashes($row['KM']);
+     echo "</strong><br />Time [s]: ";
+     echo stripslashes($row['Seconds']);
+     echo "<br />Speed [km/h]: ";
+     echo stripslashes($row['KMH']);
+     echo "</p>";
+  }
+
+  $result->free();
+  $db->close();
+
+?>
+</body>
+</html>
