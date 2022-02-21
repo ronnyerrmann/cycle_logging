dff762835d3f8dceece3ca365cf42c6789b563de
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2022-02-21 21:31:29 +0000

    Using different classes properly in java

--- ConnectionMYSQL.java
+++ ConnectionMYSQL.java
@@ -2,22 +2,22 @@ package javaCycle;
 import java.sql.*;
 import java.io.*;
 import java.util.*;
-import stringModifications.StringModifications;
+//import stringModifications.StringModifications;   // doesn't need the import, if included, then compilation of Fahrrad.java fails, probably as in a folder
 // javac -d . -classpath /usr/share/java/mysql-connector-java-8.0.27.jar:. ConnectionMYSQL.java
 
 public class ConnectionMYSQL {
-    Dictionary mysql_Settings = new Hashtable();
+    Dictionary mysqlSettings = new Hashtable();
     
-    static final String mysqlsettingsfile = "fahrrad_mysql.params";
+    static final String mysqlSettingsFile = "fahrrad_mysql.params";
     //static final Connection conn;
     Connection conn;
     String DB_URL;
     {   // This code is executed before every constructor.
         // fill array with dummy values
-        mysql_Settings.put("host", "localhost");
-        mysql_Settings.put("user", "yourusername");
-        mysql_Settings.put("password", "yourpassword");
-        mysql_Settings.put("db", "");
+        mysqlSettings.put("host", "localhost");
+        mysqlSettings.put("user", "yourusername");
+        mysqlSettings.put("password", "yourpassword");
+        mysqlSettings.put("db", "");
     }
     
     static final String QUERY = "SELECT Date, DayKM, DaySeconds, TotalKM, TotalSeconds FROM fahrrad_rides";
@@ -49,21 +49,21 @@ public class ConnectionMYSQL {
         return content;
     }
     
-    public void Load_settings(String filename){
+    public void LoadSettings(String filename){
         List<String> content = Load_File(filename);
-        List<ArrayList<String>> content_split = StringModifications.SplitStringTrim(content, "=");
-        for (int ii=0; ii<content_split.size(); ii++){
-            ArrayList<String> key_value = content_split.get(ii);
-            this.mysql_Settings.put(key_value.get(0).trim(), key_value.get(1).trim());  // trim(): remove leading and trailing whitespace
+        List<ArrayList<String>> contentSplit = StringModifications.SplitStringTrim(content, "=");
+        for (int ii=0; ii<contentSplit.size(); ii++){
+            ArrayList<String> key_value = contentSplit.get(ii);
+            this.mysqlSettings.put(key_value.get(0).trim(), key_value.get(1).trim());  // trim(): remove leading and trailing whitespace
         }
     }
     
     public Connection ConnectionMYSQL_aa()     // was a try that didn't work
     {
-        DB_URL = "jdbc:mysql://" + this.mysql_Settings.get("host");
-        if (this.mysql_Settings.get("db") != "") {DB_URL += "/" + this.mysql_Settings.get("db");}
+        DB_URL = "jdbc:mysql://" + this.mysqlSettings.get("host");
+        if (this.mysqlSettings.get("db") != "") {DB_URL += "/" + this.mysqlSettings.get("db");}
        
-        try(Connection temp_conn = DriverManager.getConnection(DB_URL, (String)mysql_Settings.get("user"), (String)mysql_Settings.get("password"));
+        try(Connection temp_conn = DriverManager.getConnection(DB_URL, (String)mysqlSettings.get("user"), (String)mysqlSettings.get("password"));
             Statement stmt = temp_conn.createStatement();
             //return temp_conn; // illegal
         ) {
@@ -75,12 +75,12 @@ public class ConnectionMYSQL {
         
     }
     
-    public Connection ConnectionMYSQL()
+    public Connection InitialiseConnectionMYSQL()
     {
-        DB_URL = "jdbc:mysql://" + this.mysql_Settings.get("host");
-        if (this.mysql_Settings.get("db") != "") {DB_URL += "/" + this.mysql_Settings.get("db");}
+        DB_URL = "jdbc:mysql://" + this.mysqlSettings.get("host");
+        if (this.mysqlSettings.get("db") != "") {DB_URL += "/" + this.mysqlSettings.get("db");}
         try {
-            Connection temp_conn = DriverManager.getConnection(DB_URL, (String)mysql_Settings.get("user"), (String)mysql_Settings.get("password"));
+            Connection temp_conn = DriverManager.getConnection(DB_URL, (String)mysqlSettings.get("user"), (String)mysqlSettings.get("password"));
             Statement stmt = temp_conn.createStatement();
             return temp_conn;
         } catch (SQLException e) {
@@ -88,6 +88,34 @@ public class ConnectionMYSQL {
             return null;
         }
         
+    }
+    
+    public ResultSet MYSQLQuery(Statement stmt, String QUERY){
+        try (ResultSet rs = stmt.executeQuery(QUERY);) {
+            return rs;
+        } catch (SQLException e) {
+            System.out.println("The following command failed: "+QUERY);
+            System.out.println("with:");
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
+    public Boolean MYSQLExecute(Statement stmt, String QUERY){
+        // stmt.executeUpdate(QUERY);
+        return true;
+        /*  // Below doesn't work:
+            // error: the try-with-resources resource must either be a variable declaration or an expression denoting a reference to a final or effectively final variable
+        final Statement stmt1 = stmt;
+        final String QUERY_l = QUERY;
+        try (stmt1.executeUpdate(QUERY_l);) {
+            return true;
+        } catch (SQLException e) {
+            System.out.println("The following command failed: "+QUERY);
+            System.out.println("with:");
+            e.printStackTrace();
+            return false;
+        }*/
     }
     
     public static void main (String[] args)
@@ -106,12 +134,12 @@ public class ConnectionMYSQL {
     public void run (String[] args) throws Exception
     {
         
-        Load_settings(mysqlsettingsfile);       // will be stored in mysql_Settings and replace standard values
+        LoadSettings(mysqlSettingsFile);       // will be stored in mysqlSettings and replace standard values
         
-        /*Enumeration enu = mysql_Settings.keys();    // Creating an empty enumeration to store
+        /*Enumeration enu = mysqlSettings.keys();    // Creating an empty enumeration to store
         while (enu.hasMoreElements()) {
             String key = (String)enu.nextElement();     // otherwise it would call nextElement twice when printing key and value
-            System.out.println( key + ":" +  mysql_Settings.get(key) );
+            System.out.println( key + ":" +  mysqlSettings.get(key) );
         }*/
         
         // Open a connection
@@ -121,14 +149,14 @@ public class ConnectionMYSQL {
         /*Connection conn = ConnectionMYSQL_aa();
         Statement stmt = conn.createStatement();    // fails, as the connection is already closed       */
         // 202202020: trying to get conn back from ConnectionMYSQL();
-        Connection conn = ConnectionMYSQL();
-        Statement stmt = conn.createStatement();    // fails, as the connection is already closed
+        Connection conn = InitialiseConnectionMYSQL();
+        Statement stmt = conn.createStatement();
         
         System.out.print("222: ");
         // ConnectionMYSQL();                      // this could replace the try - catch below
-        DB_URL = "jdbc:mysql://" + mysql_Settings.get("host");
-        if (mysql_Settings.get("db") != "") {DB_URL += "/" + mysql_Settings.get("db");}
-        try(Connection conn1 = DriverManager.getConnection(DB_URL, (String)mysql_Settings.get("user"), (String)mysql_Settings.get("password"));
+        DB_URL = "jdbc:mysql://" + mysqlSettings.get("host");
+        if (mysqlSettings.get("db") != "") {DB_URL += "/" + mysqlSettings.get("db");}
+        try(Connection conn1 = DriverManager.getConnection(DB_URL, (String)mysqlSettings.get("user"), (String)mysqlSettings.get("password"));
             Statement stmt1 = conn.createStatement();
             ResultSet rs = stmt.executeQuery(QUERY);) {
             // Extract data from result set
--- Fahrrad.java
+++ Fahrrad.java
@@ -3,59 +3,14 @@ import java.io.*;
 import java.util.*;
 import java.time.LocalDate;
 import java.time.Month;
-import javaCycle.ConnectionMYSQL;
-import javaCycle.StringModifications;
+import javaCycle.ConnectionMYSQL;       // necessary when running java Fahrrad.java
+import javaCycle.StringModifications;   // necessary when running java Fahrrad.java
 // javac -d . StringModifications.java && javac -d . -classpath /usr/share/java/mysql-connector-java-8.0.27.jar:. ConnectionMYSQL.java && java -classpath /usr/share/java/mysql-connector-java-8.0.27.jar:. Fahrrad.java
+// javac -d . StringModifications.java && javac -d . ConnectionMYSQL.java && java -classpath /usr/share/java/mysql-connector-java-8.0.27.jar:. Fahrrad.java
 
 public class Fahrrad {
-    Dictionary mysql_Settings = new Hashtable();
     Dictionary fahrrad_values = new Hashtable();
-    
-    static final String mysqlsettingsfile = "fahrrad_mysql.params";
-    //static final Connection conn;
-    String DB_URL;
-    {   // This code is executed before every constructor.
-        mysql_Settings.put("host", "localhost");
-        mysql_Settings.put("user", "yourusername");
-        mysql_Settings.put("password", "yourpassword");
-        mysql_Settings.put("db", "");
-    }
-    
-    public List<String> Load_File(String filename){
-        File file = new File(filename);
-        List<String> content = new ArrayList<String>();
-        try (FileInputStream input = new FileInputStream(file)) {
-            // create an empty string builder to store string
-           
-            // create an object of bufferedReader to read  lines from file
-            BufferedReader br = new BufferedReader(new InputStreamReader(input));
- 
-            String line;
-            // store each line one by one until reach end of file
- 
-            while ((line = br.readLine()) != null) {
-                // append string builder with line and with '/n' or '/r' or EOF
-                content.add(line);
-            }
-            //System.out.println(content);            // print string builder object i.e content
-        }
- 
-        // Catch block to handle exceptions
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        return content;
-    }
-    
-    public void Load_settings(String filename){
-        List<String> content = Load_File(filename);
-        List<ArrayList<String>> content_split = StringModifications.SplitStringTrim(content, "=");
-        for (int ii=0; ii<content_split.size(); ii++){
-            ArrayList<String> key_value = content_split.get(ii);
-            this.mysql_Settings.put(key_value.get(0).trim(), key_value.get(1).trim());  // trim(): remove leading and trailing whitespace
-        }
-    }
+    static final String mysqlSettingsFile = "fahrrad_mysql.params";
 
     public static void main (String[] args)
     {
@@ -69,51 +24,12 @@ public class Fahrrad {
             e.printStackTrace ();
         }
     }
-
-    public Statement Initialise_MYSQL_connection(String DB_URL){
-        try (Connection conn = DriverManager.getConnection(DB_URL, (String)mysql_Settings.get("user"), (String)mysql_Settings.get("password"));
-            Statement stmt = conn.createStatement();) {
-            return stmt;
-        } catch (SQLException e) {
-            System.out.println("The database connection failed with:");
-            e.printStackTrace();
-            return null;
-        }
-    }
-    
-    public ResultSet MYSQL_query(Statement stmt, String QUERY){
-        try (ResultSet rs = stmt.executeQuery(QUERY);) {
-            return rs;
-        } catch (SQLException e) {
-            System.out.println("The following command failed: "+QUERY);
-            System.out.println("with:");
-            e.printStackTrace();
-            return null;
-        }
-    }
-    
-    public Boolean MYSQL_execute(Statement stmt, String QUERY){
-        // stmt.executeUpdate(QUERY);
-        return true;
-        /*  // Below doesn't work:
-            // error: the try-with-resources resource must either be a variable declaration or an expression denoting a reference to a final or effectively final variable
-        final Statement stmt1 = stmt;
-        final String QUERY_l = QUERY;
-        try (stmt1.executeUpdate(QUERY_l);) {
-            return true;
-        } catch (SQLException e) {
-            System.out.println("The following command failed: "+QUERY);
-            System.out.println("with:");
-            e.printStackTrace();
-            return false;
-        }*/
-    }
     
     public static boolean isNumeric(String str) {
         return str != null && str.matches("[-+]?\\d*\\.?\\d+");
     }
     
-    public void get_fahrrad_values() {
+    public void getFahrradValues() {
         // Get date
         LocalDate currentdate = LocalDate.now();        // Getting the current date value
         int currentDay = currentdate.getDayOfMonth();   // Getting the current day
@@ -280,35 +196,24 @@ public class Fahrrad {
 
     public void run (String[] args) throws Exception
     {
-        
-        Load_settings(mysqlsettingsfile);       // will be stored in mysql_Settings and replace standard values
-        
-        /*Enumeration enu = mysql_Settings.keys();    // Creating an empty enumeration to store
-        while (enu.hasMoreElements()) {
-            String key = (String)enu.nextElement();     // otherwise it would call nextElement twice when printing key and value
-            System.out.println( key + ":" +  mysql_Settings.get(key) );
-        }*/
-        
         // Open a connection
-        Class.forName("com.mysql.cj.jdbc.Driver");      // java -classpath /usr/share/java/mysql-connector-java-8.0.27.jar Connection_MYSQL.java
-        DB_URL = "jdbc:mysql://" + mysql_Settings.get("host");
-        if (mysql_Settings.get("db") != "") {DB_URL += "/" + mysql_Settings.get("db");}
+        ConnectionMYSQL objMYSQL = new ConnectionMYSQL();
+        objMYSQL.LoadSettings(mysqlSettingsFile);
+        Connection conn = objMYSQL.InitialiseConnectionMYSQL();
+        if (conn == null) {
+            System.out.println("Connecting to the Database failed -> Exit");
+            System.exit(1);
+        }
+        Statement stmt = conn.createStatement();
         String QUERY = "SELECT Date, DayKM, DaySeconds, TotalKM, TotalSeconds FROM fahrrad_rides";
-        Statement stmt = Initialise_MYSQL_connection(DB_URL);        // stmt is still closed, as the try-catch block is finished
-        if (stmt == null) {System.exit(1); }
-        // recreating the connection, as Initialise_MYSQL_connection doesn't keep it open
-        Connection conn = DriverManager.getConnection(DB_URL, (String)mysql_Settings.get("user"), (String)mysql_Settings.get("password"));
-        stmt = conn.createStatement();
-        ResultSet rs = MYSQL_query(stmt, QUERY);
-        if (rs == null) {System.exit(1); }
-        // Extract data from result set
-        //while (rs.next()) {
-            // Retrieve by column name
-        //    System.out.print("ID: " + rs.getInt("id"));
-        //    System.out.print(", First: " + rs.getString("first"));
-        //}
+        ResultSet rs = objMYSQL.MYSQLQuery(stmt, QUERY);
+        if (rs == null) {
+            System.out.println("Executing of "+QUERY+" failed -> Exit");
+            System.exit(1);
+        }
         
-        get_fahrrad_values();
+        // Userinput of the values
+        getFahrradValues();
         // check that TotalKM or TotalSeconds not yet in database
         //check_values_with_db(rs);
         
