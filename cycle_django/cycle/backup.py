8ef0951d69efdb4cb642d2975a1e1878377817a6
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2023-06-20 15:49:24 +0100

    Clear old backup files

--- cycle_django/cycle/backup.py
+++ cycle_django/cycle/backup.py
@@ -1,5 +1,7 @@
-import gzip
 import datetime
+import glob
+import gzip
+import os
 import cycle.models
 from my_base import Logging, create_folder_if_required
 
@@ -27,11 +29,52 @@ def handle_object_save(sender, instance, created, **kwargs):
             print(111, obj)"""
 
 
+def remove_old_files(folder_path):
+    files = glob.glob(os.path.join(folder_path, "*.csv.gz"))
+    files.sort()
+
+    keep_files = set()
+    dates_covered = set()
+    today = datetime.datetime.now()
+    beginning_of_last_month = (today - datetime.timedelta(days=31)).replace(day=1, hour=0, minute=0, second=0)
+    one_week_ago = today - datetime.timedelta(days=7)
+
+    # Keep the last 10 files
+    keep_files.update(files[-10:])
+
+    for file in files:
+        file_name = os.path.basename(file)
+        file_date = datetime.datetime.strptime(file_name[:15], "%Y%m%d_%H%M%S")
+
+        # For files older than 1 month, keep the first file of the month
+        if file_date < beginning_of_last_month:
+            month_start = file_date.replace(day=1, hour=0, minute=0, second=0)
+            if month_start not in dates_covered:
+                dates_covered.add(month_start)
+                keep_files.add(file)
+
+        # For files older than 1 week, keep the first file of a day
+        elif file_date < one_week_ago:
+            day_start = file_date.replace(hour=0, minute=0, second=0)
+            if day_start not in dates_covered:
+                dates_covered.add(day_start)
+                keep_files.add(file)
+
+        else:
+            keep_files.add(file)
+
+    files_to_remove = set(files) - keep_files
+    logger.info(f"Clean the old files: {files_to_remove}")
+    for file_to_remove in files_to_remove:
+        os.remove(file_to_remove)
+
 def backup_db():
     create_folder_if_required(BACKUP_FOLDER)
+    remove_old_files(BACKUP_FOLDER)
 
-    with gzip.open(f"{BACKUP_FOLDER}/{datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')}.csv.gz", "w") as f:
+    with gzip.open(os.path.join(BACKUP_FOLDER, datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S.csv.gz')), "w") as f:
         for obj in cycle.models.FahrradRides.objects.all():
             f.write(f"{obj.date.strftime('%Y-%m-%d')};{obj.daykm};{obj.dayseconds};{obj.totalkm};{obj.totalseconds}"
                     f"\n".encode())
+
 
---
+++ cycle_django/cycle/tests/test_backup.py
@@ -0,0 +1,56 @@
+import random
+from datetime import date, datetime
+from django.test import TestCase
+from unittest.mock import call, MagicMock, mock_open, patch
+
+from cycle.backup import remove_old_files, backup_db, BACKUP_FOLDER
+
+MODULE_PATH = "cycle.backup."
+
+
+@patch(MODULE_PATH + "os.remove")
+@patch(MODULE_PATH + "glob.glob")
+class TestRemoveFiles(TestCase):
+
+    def test_remove_files(self, _glob, _remove):
+        file_list = ["bd/20230404_123456.csv.gz", "bd/20230404_234500.csv.gz", "bd/20230406_123456.csv.gz",
+                     "bd/20230504_123456.csv.gz", "bd/20230504_234500.csv.gz", "bd/20230506_123456.csv.gz",
+                     "bd/20230601_012345.csv.gz"] + [f"bd/20230602_0{ii}2345.csv.gz" for ii in range(10)]
+        random.shuffle(file_list)
+        _glob.return_value = file_list
+
+        with patch(MODULE_PATH + "datetime.date") as mock_date:
+            # This is required instead of a @patch
+            mock_date.today.return_value = date(2023, 6, 20)
+            mock_date.side_effect = lambda *args, **kw: date(*args, **kw)
+
+            remove_old_files("bd")
+
+        _glob.assert_called_once_with("bd/*.csv.gz")
+        self.assertEqual(
+            sorted([
+                call("bd/20230404_234500.csv.gz"), call("bd/20230406_123456.csv.gz"), call("bd/20230504_234500.csv.gz")
+            ]), sorted(_remove.call_args_list)
+        )
+
+
+@patch("cycle.models.FahrradRides.objects.all", MagicMock(return_value=[]))     # write no data for now
+@patch(MODULE_PATH + "remove_old_files")
+@patch(MODULE_PATH + "create_folder_if_required")
+class TestBackupDB(TestCase):
+
+    def test(self, _create_folder_if_required, _remove_old_files):
+        m = mock_open()
+
+        with patch(MODULE_PATH + "gzip.open", m):
+            with patch(MODULE_PATH + "datetime.datetime") as mock_datetime:
+                # This is required instead of a @patch
+                mock_datetime.utcnow.return_value = datetime(2023, 6, 20, 12, 23, 34)
+                mock_datetime.side_effect = lambda *args, **kw: date(*args, **kw)
+
+                backup_db()
+
+        m.assert_called_once_with("backup_database/20230620_122334.csv.gz", "w")
+
+        _create_folder_if_required.assert_called_once_with(BACKUP_FOLDER)
+        _remove_old_files.assert_called_once_with(BACKUP_FOLDER)
