600f4b639a4cdd3898b9c9a851c3e3c82f0c6e07
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2023-12-30 12:21:43 +0000

    Change of operating system

--- cycle_django/startup.sh
+++ cycle_django/startup.sh
@@ -1,6 +1,6 @@
-. ~/.virtualenvs/my_django_environment/bin/activate
-sleep 2
 cd ~/Documents/Scripts/cycle_logging/cycle_django
+. ../cycle_django_env/bin/activate
+sleep 2
 # Start with 0.0.0.0:8000 to listens on the network interface
 # Start with --insecure to lazily not host static files in a webserver
 python3 manage.py runserver 0.0.0.0:8000
