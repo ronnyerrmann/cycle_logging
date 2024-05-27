07282b7c21606a2c4211e51dec93e7c9d95b5dae
ronnyerrmann <36272471+ronnyerrmann@users.noreply.github.com>
2024-05-27 20:12:00 +0100

    Adjust startup script to mount point changes

--- cycle_django/startup.sh
+++ cycle_django/startup.sh
@@ -1,7 +1,7 @@
 cd ~/Documents/Scripts/cycle_logging/cycle_django
 . ../cycle_django_env/bin/activate
-export SRTM1_DIR=/media/ronny/Backup500GB/srtm/1/   # to load srtm data. If not set the gpx files won't be stored with srtm elevation
-export SRTM3_DIR=/media/ronny/Backup500GB/srtm/3/
+export SRTM1_DIR=/mnt/backup500/srtm/1/   # to load srtm data. If not set the gpx files won't be stored with srtm elevation
+export SRTM3_DIR=/mnt/backup500/srtm/3/
 sleep 2
 # Start with 0.0.0.0:8000 to listens on the network interface
 # Start with --insecure to lazily not host static files in a webserver
